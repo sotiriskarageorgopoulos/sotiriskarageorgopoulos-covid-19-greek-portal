ffe93f222baad0f061670f7cb569d123213c3642
sotiriskarageorgopoulos <sotiriskarageorgopoulos@gmail.com>
2021-04-28 13:00:56 +0300

    final project!

--- src/components/error/error.js
+++ src/components/error/error.js
@@ -1,7 +1,24 @@
 import React from 'react-router';
+import Box from '@material-ui/core/Box';
+import {Link} from 'react-router-dom'; 
 import './error.css';
 
 const Error = () => {
+    return (
+        <div className="container-fluid mt-5 distance-from-footer">
+            <div className="row">
+                <div className="col-sm-4"></div>
+                <div className="col-sm-4">
+                    <Box className="error-box">
+                        <p className="error-title">Εrror 404...</p>
+                        <p>H σελίδα δεν βρέθηκε...</p>
+                        <p>Ανακατεύθυνση στην <Link to="/home">αρχική σελίδα</Link></p>
+                    </Box>
+                </div>
+                <div className="col-sm-4"></div>
+            </div>
+        </div>
+    )
 
 }
 
